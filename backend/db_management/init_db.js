8de4cc06fefda8a68128a186595a58ff1a897411
Raymond Chow <ravingrabbit75@gmail.com>
2021-03-19 15:59:47 -0700

    Added globalInfo function to add global info data. Separated out connect to mongo db code into its own function.

--- backend/db_management/init_db.js
+++ backend/db_management/init_db.js
@@ -1,25 +1,58 @@
 "use strict";
 
+const { json } = require("express");
 const fs = require("fs");
 const mongo = require("mongodb");
 const mongoClient = mongo.MongoClient("mongodb://localhost:27017", {useUnifiedTopology: true});
 
 const dbName = "personal_website";
 const skillsCollName = "skills";
+const globalInfoCollName = "globalInfo";
 
 
-let initSkillsList = function() {
+function connectToDB() {
+    return mongoClient.connect();
+}
+
+
+function globalInfo(mongoObj) {
+    let db = null;
+    let globalInfoColl = null;
+
+    db = mongoObj.db(dbName);
+    globalInfoColl = db.collection(globalInfoCollName);
+    console.log(`Deleting all docs in ${globalInfoCollName} collection...`);
+
+    return globalInfoColl.deleteMany({}).then((delResults)=>{
+
+        console.log(`Collection Name: ${globalInfoCollName}, Deleted Docs: ${delResults.deletedCount}` );
+        let rawData = fs.readFileSync("./initial_db_data/baseContent.json");
+        let jsonData = JSON.parse(rawData);
+        console.log(`Inserting global info data...`);
+        return globalInfoColl.insertOne(jsonData);
+
+    }).then((insertResults)=>{
+
+        console.log(`Collection Name: ${globalInfoCollName}, Inserted Docs: ${insertResults.insertedCount}`);
+
+    }).catch((err)=>{
+
+        console.log(err);
+        process.exit();
+
+    });
+}
+
+
+function initSkillsList(mongoObj) {
     let db = null;
     let skillsColl = null;
 
-    return mongoClient.connect().then((mongoObj)=>{
+    db = mongoObj.db(dbName);
+    skillsColl = db.collection(skillsCollName);
+    console.log(`Deleting all docs in ${skillsCollName} collection...`);
 
-        db = mongoObj.db(dbName);
-        skillsColl = db.collection(skillsCollName);
-        console.log(`Deleting all docs in ${skillsCollName} collection...`)
-        return skillsColl.deleteMany({});
-
-    }).then((delResults)=>{
+    return skillsColl.deleteMany({}).then((delResults)=>{
 
         console.log(`Collection Name: ${skillsCollName}, Deleted Docs: ${delResults.deletedCount}` );
         let rawData = fs.readFileSync("./initial_db_data/skills.json");
@@ -41,7 +74,9 @@ let initSkillsList = function() {
 }
 
 let run = async () => {
-    await initSkillsList();
+    let mongoObj = await connectToDB();
+    await globalInfo(mongoObj);
+    await initSkillsList(mongoObj);
     mongoClient.close();
 }
 
